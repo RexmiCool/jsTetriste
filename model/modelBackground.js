233cd129a8b3a25ba702bcc2023ee334e0fb739c
GabrielM1z <gabazoo.martinez@gmail.com>
2023-01-06 14:09:14 +0100

    glow

--- model/modelBackground.js
+++ model/modelBackground.js
@@ -21,14 +21,14 @@ class Background {
 			this.ctx.beginPath();
 			this.ctx.moveTo(60,90+i*30);
 			this.ctx.lineTo(360,90+i*30);
-			this.ctx.strokeStyle = "#FF000055"
+			this.ctx.strokeStyle = "#50505055"
 			this.ctx.stroke()
 		}
 		for (let i = 1; i < 10; i++) {
 			this.ctx.beginPath();
 			this.ctx.moveTo(60+i*30,90);
 			this.ctx.lineTo(60+i*30,810);
-			this.ctx.strokeStyle = "#FF000055"
+			this.ctx.strokeStyle = "#50505055"
 			this.ctx.stroke()
 		}
 
@@ -36,6 +36,17 @@ class Background {
 		this.ctx.beginPath();
 		this.ctx.rect(60, 90, 300, 720);
 		this.ctx.strokeStyle = "#FF0000";
+		this.ctx.shadowBlur = 10;
+		this.ctx.shadowColor = "#FF0000";
+		this.ctx.stroke();
+		this.ctx.closePath();
+
+		// preview bg
+		this.ctx.beginPath();
+		this.ctx.rect(390, 120, 150, 90);
+		this.ctx.strokeStyle = "#FF0000";
+		this.ctx.shadowBlur = 10;
+		this.ctx.shadowColor = "#FF0000";
 		this.ctx.stroke();
 		this.ctx.closePath();
 
@@ -44,17 +55,14 @@ class Background {
 		this.ctx.strokeStyle = "#FF0000";
 		this.ctx.textAlign = "center";
 		this.ctx.textBaseline = "middle";
+		this.ctx.shadowBlur = 10;
+		this.ctx.shadowColor = "#FF0000";
 		this.ctx.strokeText('NEON TETRIS', 300, 45);
 		
 		// titre score
 		this.ctx.strokeText('SCORE', 465, 300);
 
-		// preview bg
-		this.ctx.beginPath();
-		this.ctx.rect(390, 120, 150, 90);
-		this.ctx.strokeStyle = "#FF0000";
-		this.ctx.stroke();
-		this.ctx.closePath();
+		
 		
 	}
 }
