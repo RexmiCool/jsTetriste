3bbf64fce02805a9c149997eab6140e2fd3d1b54
GabrielM1z <gabazoo.martinez@gmail.com>
2023-01-06 11:49:43 +0100

    inti

--- controler/controlerMain.js
+++ controler/controlerMain.js
@@ -7,11 +7,7 @@ let bg = new Background(600, 900, ctx);
 
 function draw(){
 
-	ctx.beginPath();
-	ctx.rect(20, 40, 300, 720);
-	ctx.fillStyle = "#FF0000";
-	ctx.fill();
-	ctx.closePath();
+	
 
 }
 
